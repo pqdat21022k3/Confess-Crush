3eec9a33f0328a9d0663a8b0373fa7abd6b7cbad
pqdat21022k3 <96726088+pqdat21022k3@users.noreply.github.com>
2022-01-06 14:18:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    titleWeb: "Hello",
+    introTitle: 'Công chúa của tôi',
+    introDesc: `Xin lỗi vì tối qua đã không hỏi han em, đi quay cả ngày em yêu của tôi đã mệt lắm rồi`,
+    btnIntro: 'Hic :( ',
+    title: 'Tui xin lỗi, tui thực sự biết lỗi rồi 🥰',
+    desc: 'Tha lỗi cho tôi có được không ? ',
+    btnYes: 'Được <33',
+    btnNo: 'Không :3',
+    question: 'Tại sao bạn lại tha lỗi cho tôi vậy ? ',
+    btnReply: 'Tha lỗi cho bạn cún nhá',
+    reply: 'Bạn cún biết rồi',
+    mess: 'Nay thực tập về mệt rồi 😘😘',
+    messDesc: 'Gọi xíu nhaaaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/phamquocdatdeptrainhattrendoi/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
